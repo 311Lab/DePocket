2cd1ba380ddfd533818e96c8192347933189e997
Tianfei Yu <156671335+311Lab@users.noreply.github.com>
2025-07-06 10:09:52 +0800

    Update run_MPNN.py

--- example/6_sequenceGenerate/run_MPNN.py
+++ example/6_sequenceGenerate/run_MPNN.py
@@ -4,10 +4,10 @@ import os
 import subprocess
 
 def run_sbatch():
-    # 进入指定路径
+    
     os.chdir('../../../ProteinMPNN-main/examples')
 
-    # 激活conda环境并提交sbatch作业
+    
     subprocess.run('bash -i -c "conda activate mlfold && sbatch submit_example_2.sh"', shell=True, check=True)
 
 if __name__ == "__main__":
